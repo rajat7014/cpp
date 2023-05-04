8ac43d1cb67b34f6857650d0c90edba455487ea8
rajat7014 <132184024+rajat7014@users.noreply.github.com>
2023-05-04 21:47:44 +0530

    Create forloop.cpp

---
+++ forloop.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    
+    string st[9] = {"one","two","three","four","five","six","seven","eight","nine"};
+    int a;
+    int b;
+    cin>>a>>b;
+    for(int i=a; i<=b;i++){
+      if(i<=9){
+          cout<<st[i-1]<<endl;
+      }
+      else{
+          if(i%2==0){
+              cout<<"even"<<endl;
+          }
+          else if(i%2!=0){
+              cout<<"odd"<<endl;
+          }
+      }
+    }
+    return 0;
+}
