b44dc56496b60a64f79a7a01a97f0a6ac2731ce3
rajat7014 <132184024+rajat7014@users.noreply.github.com>
2023-05-09 23:37:05 +0530

    Add files via upload

---
+++ assignment/p6.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"enter any two numbers";
+    cin>>a>>b;
+    if(a==b){
+        cout<<"A and B are equal";
+    }
+    else{
+        cout<<"A and B are no tequal";
+    }
+    return 0;
+}
